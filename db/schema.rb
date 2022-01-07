677fca31c7ffe01641478eeb9286d958f0e6f0f6
Hanada-Takao <taaa0823@gmail.com>
2022-01-07 18:18:23 +0900

    session_login_add

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2022_01_07_073515) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "table_blogs", force: :cascade do |t|
     t.string "string"
     t.text "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -28,6 +26,6 @@ ActiveRecord::Schema.define(version: 2022_01_07_073515) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-
   end
+
 end
